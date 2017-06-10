b690ba4be2b14b96ee1db13c943b3a9c0ae54ea8
Matthew Downey <mattddowney@gmail.com>
2017-06-10 16:19:35 -0600

    add results to table

--- html/js/app.js
+++ html/js/app.js
@@ -4,6 +4,25 @@ var CITY_QUERY_URL_PREFIX = 'http://localhost:3000/city?name=ilike.'
 
 var app = {};
 
+app.addResultRows = function (rows) {
+    var rowsString = '';
+
+    for (var i = 0, max = rows.length; i < max; i++) {
+        rowsString += app.buildResultRowString(rows[i]);
+    }
+
+    document.getElementById('results-table-body').innerHTML = rowsString;
+}
+
+app.buildResultRowString = function (row) {
+    return  '<tr>' +
+                '<td>' + row.name + '</td>' +
+                '<td>' + row.district + '</td>' +
+                '<td>' + row.countrycode + '</td>' +
+                '<td>' + row.population + '</td>' +
+            '</tr>';
+}
+
 app.queryCity = function (cityName) {
     var queryURL = CITY_QUERY_URL_PREFIX + '*' + cityName + '*';
     
@@ -13,5 +32,6 @@ app.queryCity = function (cityName) {
         })
         .then(function (j) {
             console.log(j);
+            app.addResultRows(j);
         })
 }
